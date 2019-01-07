763faf5b7f9b3f7abd5f9198a5b26796221dafb2
dulanis <saagardulani96@gmail.com>
2019-01-07 16:21:34 -0500

    Added test objects

--- script.py
+++ script.py
@@ -1,1 +1,5 @@
-#Tourism Recommendation Engine
+# Tourism Recommendation Engine
+destinations = ["Paris, France", "Shanghai, China",
+                "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
+test_traveler = ["Erin Wilkes", "Shanghai, China", ["historical site", "art"]]
+print(destinations)
